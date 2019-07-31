446a2a108fb37ec88c55ad407f25f54bc287fbed
arnold bwaila <arnold.bwaila@gmail.com>
2019-07-31 21:50:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  count = array.length
+  count = 0
+  arr_lnth = array.length
+  
+  while count < arr_lnth do
+    array.index(count) = array.index(count)**2
+    count++
+  
+  
+  
 end
